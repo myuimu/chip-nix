d6deaa497f49930e6781d623b1944ddbbbc1c6a0
Nick Goodson <nick_goodson@unigroup.com>
2024-03-11 17:46:58 -0500

    update commit rev?

--- default.nix
+++ default.nix
@@ -13,7 +13,7 @@
     src = config.deps.fetchFromGitHub {
       owner = "myuimu";
       repo = "chip-nix";
-      rev = "27fcdbd3adf032d8b9314b11ea8c3a6e0481252a";
+      rev = "c7db37fa3e2a741d14ffec45ab0971e9daba24d1";
       sha256 = "sha256-3BM17MwfUQwrUc/JLsmxTob5wSey5xmsHUdG1dOcqOU=";
     };
   };
